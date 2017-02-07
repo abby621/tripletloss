646ca7822ba0644a0252e563d084b0bc9d791f11
abby <sw62190>
2017-02-07 14:52:06 -0600

    debug

--- tripletloss/train.py
+++ tripletloss/train.py
@@ -61,8 +61,8 @@ class SolverWrapper(object):
             timer.tic()
             self.solver.step(1)
             #print self.solver.net.params.keys()
-            #print 'fc9_1:',sorted(self.solver.net.params['fc9_1'][0].data[0])[-1]
-            print 'fc9:',self.solver.net.params['fc9'][0].data[0]
+            print 'fc9_1:',sorted(self.solver.net.params['fc9_1'][0].data[0])[-1]
+            # print 'fc9:',self.solver.net.params['fc9'][0].data[0]
             #print 'fc7:',sorted(self.solver.net.params['fc7'][0].data[0])[-1]
             #print 'fc6:',sorted(self.solver.net.params['fc6'][0].data[0])[-1]
             #print 'fc9:',(self.solver.net.params['fc9'][0].data[0])[0]
