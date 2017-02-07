b632d05f932e649f56e39f10665fa866aa69612c
abby <sw62190>
2017-02-07 13:07:48 -0600

    Handlw cases without enough images for triplets

--- tripletloss/datalayer.py
+++ tripletloss/datalayer.py
@@ -31,17 +31,18 @@ class DataLayer(caffe.Layer):
         if self._index >= len(self.data_container._train_im_paths):
             self._index = 0
 
-        train_im_path = self.data_container._train_im_paths[self._index]
-        train_im_label = self.data_container._train_im_labels[self._index]
-
-        self._index = self._index + 1
-        while len(sample) < self._triplet:
-            sample.append(train_im_path)
-            sample_labels.append(train_im_label)
-
-        positive_examples = [i for i,x in enumerate(self.data_container._train_im_labels) if x==train_im_label and i!=self._index]
-        negative_examples = [i for i,x in enumerate(self.data_container._train_im_labels) if x!=train_im_label]
-
+        positive_examples = []
+        negative_examples = []
+        while len(positive_examples) < 2 or len(negative_examples) < 2:
+            train_im_path = self.data_container._train_im_paths[self._index]
+            train_im_label = self.data_container._train_im_labels[self._index]
+            self._index = self._index + 1
+            while len(sample) < self._triplet:
+                sample.append(train_im_path)
+                sample_labels.append(train_im_label)
+            positive_examples = [i for i,x in enumerate(self.data_container._train_im_labels) if x==train_im_label and i!=self._index]
+            negative_examples = [i for i,x in enumerate(self.data_container._train_im_labels) if x!=train_im_label]
+        
         # Sample positive samples
         while len(sample) < self._triplet*2:
             pos_index = random.randint(0,len(positive_examples)-1)
