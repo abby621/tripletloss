b9eb41cfcab9ec2c06002e11d4f0b5264b78488a
abby <sw62190>
2017-02-07 14:12:45 -0600

    Mean subtraction fix

--- tripletloss/config.py
+++ tripletloss/config.py
@@ -30,4 +30,4 @@ blob = caffe_pb2.BlobProto()
 data = open('/project/focus/datasets/tc_tripletloss/mean.binaryproto', 'rb' ).read()
 blob.ParseFromString(data)
 arr = np.array(blobproto_to_array(blob))
-IM_MEAN = arr[0]
+IM_MEAN = arr[0].reshape(256,256,3)
