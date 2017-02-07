55d72a8e56db4941b59a306b1b73afcd312894e9
abby <sw62190>
2017-02-07 10:09:32 -0600

    Typos

--- tripletloss/hoteldata.py
+++ tripletloss/hoteldata.py
@@ -5,7 +5,7 @@ import csv
 
 class hoteldata():
     global _train_im_paths
-    global _train_labels
+    global _train_im_labels
     global _val_im_paths
     global _val_labels
 
@@ -14,7 +14,7 @@ class hoteldata():
             readerT = csv.reader(tf, delimiter=' ')
             trainIms = list(readerT)
         self._train_im_paths = [i[0] for i in trainIms]
-        self._train_labels = [i[1] for i in trainIms]
+        self._train_im_labels = [i[1] for i in trainIms]
 
         with open(cfg.VAL_FILE,'rU') as vf:
             readerV = csv.reader(vf, delimiter=' ')
@@ -25,7 +25,7 @@ class hoteldata():
         if cfg.FLIPPED:
             flipped_train_ims = [i.split('.')[0]+'_flip.jpg' for i in self._train_im_paths]
             self._train_im_paths.extend(flipped_train_ims)
-            self._train_labels.extend(self._train_labels)
+            self._train_im_labels.extend(self._train_im_labels)
 
             flipped_val_ims = [i.split('.')[0]+'_flip.jpg' for i in self._val_im_paths]
             self._val_im_paths.extend(flipped_val_ims)
