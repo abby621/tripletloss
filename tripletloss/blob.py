18f1c0c0774784170a08b59912e507a1a0a02da4
abby <sw62190>
2017-05-03 10:17:22 -0500

    Change scaling for mnist

--- tripletloss/blob.py
+++ tripletloss/blob.py
@@ -27,8 +27,7 @@ def im_list_to_blob(ims):
     return blob
 
 def prep_im_for_blob(im):
-    target_size = 224
     im = im.astype(np.float32, copy=False)
     im -= cfg.IM_MEAN
-    im = cv2.resize(im, (224,224), interpolation=cv2.INTER_LINEAR)
+    im = cv2.resize(im, (config.TARGET_SIZE,config.TARGET_SIZE), interpolation=cv2.INTER_LINEAR)
     return im
--- tripletloss/config.py
+++ tripletloss/config.py
@@ -36,12 +36,20 @@ FLIPPED = True
 
 blob = caffe_pb2.BlobProto()
 # data = open('/project/focus/datasets/tc_tripletloss/mean.binaryproto', 'rb' ).read()
+# blob.ParseFromString(data)
+# arr = np.array(blobproto_to_array(blob))
+# IM_MEAN = arr[0].mean(1).mean(1)
+
+# TARGET_SIZE = 224
+# CROP_SZ = 227
+
 data = open('/project/focus/datasets/mnist/mnist_mean.binaryproto','rb').read()
 blob.ParseFromString(data)
 arr = np.array(blobproto_to_array(blob))
 IM_MEAN = arr[0].mean(1).mean(1)
 
-CROP_SZ = 227
+TARGET_SIZE = 28
+CROP_SZ = 28
 
 TRIPLET_TRAINING = False # if we're not fine tuning with triplet loss, this should be false
 
