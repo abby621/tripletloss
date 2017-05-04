5a543defe2a5fa1fadaed55bf079f2ee72f75f7f
abby <sw62190>
2017-05-04 09:51:52 -0500

    Typo

--- tripletloss/datalayer.py
+++ tripletloss/datalayer.py
@@ -139,7 +139,6 @@ class DataLayer(caffe.Layer):
             sample.append(shuffled_im_paths[n])
             sample_labels.append(shuffled_im_labels[n])
 
-        print sample
         im_blob = self._get_image_blob(sample)
         blobs = {'data': im_blob,
              'labels': sample_labels}
