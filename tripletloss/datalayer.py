8a493dc1ddd3a86969a23abfaa5b1c3c28539952
abby <sw62190>
2017-05-09 15:44:55 -0500

    Only use the triplet order when doing triplet loss, otherwise select images randomly

--- tripletloss/datalayer.py
+++ tripletloss/datalayer.py
@@ -143,9 +143,9 @@ class DataLayer(caffe.Layer):
             for r in rand_order:
                 sample.append(shuffled_im_paths[r])
                 sample_labels.append(shuffled_im_labels[r])
-            print sample_labels
 
         im_blob = self._get_image_blob(sample)
+        print im_blob
         blobs = {'data': im_blob,
                  'labels': sample_labels}
         return blobs
