9c2e97519555a0be8246ff6fe9dcbefd194dc14f
abby <sw62190>
2017-05-24 11:37:08 -0500

    Restructuring and setting up for mnist

--- tripletloss/config.py
+++ tripletloss/config.py
@@ -13,21 +13,8 @@ if WHICH_DATASET == 'tc':
     CROP_SZ = 227
     TRAIN_BATCH_SIZE = 30
     TEST_BATCH_SIZE = 30
-    data = open('/project/focus/datasets/tc_tripletloss/mean.binaryproto', 'rb' ).read()
-elif WHICH_DATASET == 'cifar':
-    TARGET_SIZE = 32
-    CROP_SZ = 32
-    TRAIN_BATCH_SIZE = 50
-    TEST_BATCH_SIZE = 50
-    data = open('/project/focus/datasets/cifar-10/mean.binaryproto', 'rb' ).read()
 elif WHICH_DATASET == 'mnist':
     TARGET_SIZE = 28
     CROP_SZ = 28
-    TRAIN_BATCH_SIZE = 256
-    TEST_BATCH_SIZE = 50
-    data = open('/project/focus/datasets/mnist/mnist_mean.binaryproto','rb').read()
-
-blob.ParseFromString(data)
-arr = np.array(blobproto_to_array(blob))
-NUM_CHANNELS = arr.shape[1]
-IM_MEAN = arr.squeeze()
+    TRAIN_BATCH_SIZE = 270
+    TEST_BATCH_SIZE = 270
